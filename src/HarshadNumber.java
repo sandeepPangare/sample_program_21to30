5e0e2c0fd5f28ac04bc58e357fa626bb567636ba
sandeepPangare <pangaresandeep96@gmail.com>
2022-09-05 22:05:35 +0530

    some basic problem

--- src/HarshadNumber.java
+++ src/HarshadNumber.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class HarshadNumber {
+import java.util.Scanner;
+
+public class HarshadNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any Number: ");
+        int n = sc.nextInt();
+        int sum=0, temp=n;
+        while (temp!=0) {
+            int d=temp%10;
+            sum+=d;
+            temp=temp/10;
+        }
+        if (n%sum==0)
+            System.out.println(n+" is a Harshad Number");
+        else
+            System.out.println(n+" is not a Harshad Number");
+
+    }
 }
